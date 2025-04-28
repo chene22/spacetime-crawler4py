c66311b74ea4164ef8911476300ff83b5eee8304
Ethan C. <publicemailadrs@gmail.com>
2025-04-27 20:06:37 -0700

    added 2 prints inside get_cache_server

--- utils/server_registration.py
+++ utils/server_registration.py
@@ -20,7 +20,9 @@ def init(df, user_agent, fresh):
     return reg.load_balancer
 
 def get_cache_server(config, restart):
+    print("Starting get_cache_server");
     init_node = Node(
         init, Types=[Register], dataframe=(config.host, config.port))
+    print("We have defined init_node")
     return init_node.start(
         config.user_agent, restart or not os.path.exists(config.save_file))
