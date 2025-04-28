359b2c259668c33635717f57293cc2ab36c96aa2
Kyler Narvaez <dealingcardsk@gmail.com>
2025-04-27 20:00:04 -0700

    Changed USeragent

--- launch.py
+++ launch.py
@@ -17,7 +17,6 @@ def main(config_file, restart):
 
 
 if __name__ == "__main__":
-    print("Checking if thing runs")
     parser = ArgumentParser()
     parser.add_argument("--restart", action="store_true", default=False)
     parser.add_argument("--config_file", type=str, default="config.ini")
