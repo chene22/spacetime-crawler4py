468ffc998db3a59dedda5a42492926898c92f191
Kyler Narvaez <dealingcardsk@gmail.com>
2025-04-28 15:03:53 -0700

    Added /talks/date to be ignored

--- scraper.py
+++ scraper.py
@@ -77,7 +77,8 @@ def is_valid(url):
         #Foudn [^/]+ which is like a stand-in that can be some sub-path
         if re.search(
             r'(day/\d{4}-\d{2}-\d{2}|events/\d{4}-\d{2}-\d{2}' +
-            r'|/events/category/[^/]+/\d{4}-\d{2}|events/[^/]+/\d{4}-\d{2})', parsed.path):
+            r'|/events/category/[^/]+/\d{4}-\d{2}|events/[^/]+/\d{4}-\d{2})' +
+            r'|/talks/\d{4}-\d{2}-\d{2}', parsed.path):
             return False
         
         
