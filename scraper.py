8b367abca2a16d25328ce1a871115bca70b3a4d1
Kyler Narvaez <dealingcardsk@gmail.com>
2025-04-29 16:10:53 -0700

    Added more qeury restrictions

--- scraper.py
+++ scraper.py
@@ -2,14 +2,14 @@ import re
 from urllib.parse import urlparse, urljoin, urldefrag
 from bs4 import BeautifulSoup
 import json
-# from nltk.corpus import stopwords
+from nltk.corpus import stopwords
 
 longest_page = dict() # url, length in words, for finding longest page in terms of number of words excluding stopwords
 word_frequencies = dict() # word, frequency, for finding top 50 most common words
 subdomains = dict() # subdomain (ie vision.uci.edu), number of pages in it. you can count # of unique pages by summing values of this dictionary
 seen_urls = set()
 
-
+'''
 STOPWORDS = set([
     "a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't",
     "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't",
@@ -27,9 +27,9 @@ STOPWORDS = set([
     "you've", "your", "yours", "yourself", "yourselves", "&", "-", ",", ".", ":", "—", "says", "will", "b", "c", "d",
     "e", "f", "g", "h", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
 ])
+'''
 
-
-# STOPWORDS = set(stopwords.words('english'))
+STOPWORDS = set(stopwords.words('english'))
 word_filter = re.compile(r'^[a-z]+$')
 
 crawled_num = 0
@@ -100,7 +100,10 @@ def is_valid(url):
             "tribe-bar-date=",
             "eventDate=",
             "paged=",
-            "eventDisplay="
+            "eventDisplay=",
+            "do=",
+            "ns=",
+            "tab_files="
         ]
         #may be inefficient
         if any(query in parsed.query for query in unallowed_queries): #checks if parsed queries have any of the unallowed_queries
