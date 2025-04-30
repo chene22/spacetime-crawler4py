b9d5991183140563ad96e933ef7f175811e0b876
Kyler Narvaez <dealingcardsk@gmail.com>
2025-04-30 09:41:35 -0700

    Adding /-/ for gitlabs

--- scraper.py
+++ scraper.py
@@ -119,7 +119,7 @@ def is_valid(url):
         if re.search(
             r'(day/\d{4}-\d{2}-\d{2}|events/\d{4}-\d{2}-\d{2}' +
             r'|/events/category/[^/]+/\d{4}-\d{2}|events/[^/]+/\d{4}-\d{2}' +
-            r'|/talks/\d{4}-\d{2}-\d{2})', parsed.path):
+            r'|/talks/\d{4}-\d{2}-\d{2}|/-/)', parsed.path):
             return False
         
         
